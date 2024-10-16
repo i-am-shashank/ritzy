7668555567dd522d5b9c25c63cfb56dce5540aaa
this-is-pooja <poojasoliwal2222@gmail.com>
2024-10-16 18:00:44 +0530

    Handle empty search response and display 'no match found' message

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { MdAddShoppingCart } from "react-icons/md";
-import { RiArrowUpDownFill } from "react-icons/ri";
-import { Link } from "react-router-dom";
 import {
-  Center,
-  Text,
   Box,
+  Button,
+  Center,
   HStack,
   Image,
   Spacer,
-  Button,
+  Text,
 } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
-import { useLocation } from "react-router-dom";
-import { getData } from "../services/essentials";
+import { MdAddShoppingCart } from "react-icons/md";
+import { RiArrowUpDownFill } from "react-icons/ri";
 import SearchHandler from "../components/search";
+import { getData } from "../services/essentials";
+import { useLocation } from "react-router-dom";
 
 export default function Category() {
   const icon = { color: "#5E5E5E", fontSize: "1.4rem" };
@@ -64,7 +65,6 @@ export default function Category() {
               <SearchHandler />
             </Box>
             <Box display="flex">
-              {/* <Center mr="1rem" fontSize="1.2rem" color="lblack" p="0 1rem" h="2.4rem" bg="white" borderRadius="4px"><Flex>filters{<FiFilter style={{ marginTop: "0.2rem" }} />}</Flex></Center> */}
               <Button
                 onClick={onSortHandler}
                 fontSize="1.2rem"
@@ -91,7 +91,7 @@ export default function Category() {
           justifyContent="center"
         >
           {loading ? (
-            Data.map((post) => (
+            Data?.map((post) => (
               <Link
                 to={{
                   pathname: "/item",
@@ -150,59 +150,7 @@ export default function Category() {
             <Loading />
           )}
         </Box>
-      </Box>
-      {/* <Box display={{ sm: "block", md: "block", lg: "none", xl: "none" }}>
-				<Center>
-					<HStack spacing={14} bg="gray" w="100%" h="5rem" pl={{ md: "3rem", sm: "1rem" }} pr={{ md: "3rem", sm: "1rem" }} justifyContent="space-evenly">
-						<Box>
-							<SearchHandler />
-						</Box>
-						<Box display={{ sm: "none", md: "block", lg: "block", xl: "block" }}>
-							<Center fontSize="1.2rem" color="lblack" w="8.4rem" h="2.4rem" bg="white" borderRadius="4px"><Flex>filters{<FiFilter style={{ marginTop: "0.2rem" }} />}</Flex></Center>
-						</Box>
-						<Box display={{ sm: "none", md: "block", lg: "block", xl: "block" }}>
-							<Center fontSize="1.2rem" color="lblack" w="8.4rem" h="2.4rem" bg="white" borderRadius="4px"><Flex>sort by{<RiArrowUpDownFill style={{ marginTop: "0.2rem", marginLeft: "0.1rem" }} />}</Flex></Center>
-						</Box>
-						<Box  display={{ sm: "block", md: "none", lg: "none", xl: "none" }}>
-							<Menu>
-								<MenuButton
-									as={IconButton}
-									aria-label="Options"
-									icon={<BsFilter fontSize="1.5rem"/>}
-									variant="outline"
-								/>
-								<MenuList>
-									<MenuItem pl="4.8rem">
-										<Center fontSize="1.2rem" color="lblack" w={{ md: "8.4rem", }} h="2.4rem" bg="white" borderRadius="4px"><Flex>filters{<FiFilter style={{ marginTop: "0.2rem" }} />}</Flex></Center>
-									</MenuItem>
-									<MenuItem pl="4.4rem">
-										<Center fontSize="1.2rem" color="lblack" w={{ md: "8.4rem", }} h="2.4rem" bg="white" borderRadius="4px"><Flex>sort by{<RiArrowUpDownFill style={{ marginTop: "0.2rem", marginLeft: "0.1rem" }} />}</Flex></Center>
-									</MenuItem>
-								</MenuList>
-							</Menu>
-						</Box>
-					</HStack>
-				</Center>
-				<Box p={{ md: "2rem", sm: "1rem" }}>
-					{loading ? Data.map(post => (
-						<Link to={{ pathname: '/item', hash: '', search: `Name=${post.seller}&img=${post.img}&des=${post.description}&name=${post.name}&price=${post.price}&instock=${post.inStock}` }}>
-							<Box key={post.id} alignItems="center" w="15.8rem" h={{ md: "19.5rem", sm: "20.5rem" }} position="relative" style={{ borderRadius: "5px", boxShadow: "rgba(0, 0, 0, 0.15) 0px 3px 3px 0px" }} float="left" m={{ md: "1.6rem", sm: "1.4rem" }}>
-								<Image src={post.img} borderRadius="4px" />
-								<Box bg="offWhite" w="15.8rem" position="absolute" h="5rem">
-									<Box p={{ md: "0.6rem", sm: "0.4rem" }}>
-										<Text color="lblack" fontSize={{ md: "0.9rem" }} style={{ textTransform: "uppercase", letterSpacing: "0.1rem", wordSpacing: "0.1rem", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{post.name}</Text>
-									</Box>
-									<HStack pl={{ md: "0.6rem", sm: "0.4rem" }} pr={{ md: "0.6rem", sm: "0.4rem" }}>
-										<Text color="lblack" fontSize="1.1rem">${post.price}</Text>
-										<Spacer />
-										<MdAddShoppingCart style={icon} />
-									</HStack>
-								</Box>
-							</Box>
-						</Link>
-					)) : <Loading />}
-				</Box>
-			</Box> */}
+      </Box>     
     </>
   );
 }
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,85 +1,121 @@
-import React, { useState, useEffect } from "react";
-import { MdAddShoppingCart } from "react-icons/md";
+import { Box, HStack, Image, Spacer, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
 import { Link } from "react-router-dom";
-import { Text, Box, HStack, Image, Spacer } from "@chakra-ui/react";
 import Loading from "../components/Loading";
+import { MdAddShoppingCart } from "react-icons/md";
 import { useLocation } from "react-router-dom";
 
 export default function Result() {
   const icon = { color: "#5E5E5E", fontSize: "1.4rem" };
   const [Data, setData] = useState([]);
-  const [loading, setloading] = useState(false);
+  const [loading, setloading] = useState(true);
   const location = useLocation();
-  const searchResult = location.state.data;
+  const searchResult = location?.state?.data;
 
   useEffect(() => {
     setData(searchResult);
-    setloading(true);
+    setloading(false);
   }, []);
+
   return (
-    <>
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        alignItems="center"
-        justifyContent="center"
-      >
-        {loading ? (
-          Data.map((post) => (
-            <Link
-              to={{
-                pathname: "/item",
-                hash: "",
-                search: `Name=${post.seller}&img=${post.img}&des=${post.description}&name=${post.name}&price=${post.price}&instock=${post.inStock}`,
-              }}
+    <Box
+      display="flex"
+      flexWrap="wrap"
+      alignItems="center"
+      justifyContent="center"
+    >
+      {loading ? (
+        <Loading />
+      ) : Data?.length > 0 ? (
+        Data.map((post) => (
+          <Link
+            to={{
+              pathname: "/item",
+              hash: "",
+              search: `Name=${post.seller}&img=${post.img}&des=${post.description}&name=${post.name}&price=${post.price}&instock=${post.inStock}`,
+            }}
+            key={post.id}
+          >
+            <Box
+              w="18.5rem"
+              alignItems="center"
+              style={{ borderRadius: "4px" }}
+              float="left"
+              m="4rem 1rem"
+              boxShadow="rgba(0, 0, 0, 0.15) 0px 3px 3px 0px"
             >
+              <Image src={post.img} borderRadius="4px 4px 0 0" alt="image" />
               <Box
-                key={post.id}
+                bg="offWhite"
                 w="18.5rem"
-                alignItems="center"
-                style={{ borderRadius: "4px" }}
-                float="left"
-                m="4rem 1rem"
-                boxShadow="rgba(0, 0, 0, 0.15) 0px 3px 3px 0px"
+                h="5rem"
+                borderRadius="0 0 4px 4px"
               >
-                <Image src={post.img} borderRadius="4px 4px 0 0" alt="image" />
-                <Box
-                  bg="offWhite"
-                  w="18.5rem"
-                  h="5rem"
-                  borderRadius="0 0 4px 4px"
-                >
-                  <Box p="0.8rem">
-                    <Text
-                      color="lblack"
-                      fontSize="1rem"
-                      style={{
-                        textTransform: "uppercase",
-                        letterSpacing: "0.1rem",
-                        wordSpacing: "0.1rem",
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                      }}
-                    >
-                      {post.name}
-                    </Text>
-                  </Box>
-                  <HStack pl="0.8rem" pr="0.8rem">
-                    <Text color="lblack" fontSize="1.1rem">
-                      ${post.price}
-                    </Text>
-                    <Spacer />
-                    <MdAddShoppingCart style={icon} />
-                  </HStack>
+                <Box p="0.8rem">
+                  <Text
+                    color="lblack"
+                    fontSize="1rem"
+                    style={{
+                      textTransform: "uppercase",
+                      letterSpacing: "0.1rem",
+                      wordSpacing: "0.1rem",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  >
+                    {post.name}
+                  </Text>
                 </Box>
+                <HStack pl="0.8rem" pr="0.8rem">
+                  <Text color="lblack" fontSize="1.1rem">
+                    ${post.price}
+                  </Text>
+                  <Spacer />
+                  <MdAddShoppingCart style={icon} />
+                </HStack>
               </Box>
-            </Link>
-          ))
-        ) : (
-          <Loading />
-        )}
-      </Box>
-    </>
+            </Box>
+          </Link>
+        ))
+      ) : (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh"
+          w="100%"
+          textAlign="center"
+          flexDir="column"
+        >
+          <Text fontSize="4xl" fontWeight="700" color="lblack">
+            No result found
+          </Text>
+          <Text fontSize={"xl"} color="gray">
+            We can't find any item matching your search.
+          </Text>
+          <Link
+            to={{
+              pathname: "/",
+            }}
+          >
+            <Box
+              mt="1.5rem"
+              h="1.8rem"
+              as="button"
+              color="white"
+              bg="red"
+              p="0 10px"
+              transition="all 0.2s"
+              _active={{ transform: "scale(0.96)" }}
+              style={{ borderRadius: "0.2rem" }}
+            >
+              Continue Shopping
+            </Box>
+          </Link>
+        </Box>
+      )}
+    </Box>
   );
 }
