6d528d76c9d057df6cdb3a19922b87eb969e688c
this-is-pooja <poojasoliwal2222@gmail.com>
2024-10-21 00:23:32 +0530

    Added toaster for error notifications

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,8 @@
+import "react-toastify/dist/ReactToastify.css";
+
 import { Input, InputGroup, InputRightAddon } from "@chakra-ui/input";
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 import { GoSearch } from "react-icons/go";
 import { searchAll } from "../services/essentials";
@@ -18,25 +21,31 @@ export default function Search() {
         .then((res) => {
           history.push(`/result/${value}`, { data: res.result });
         })
-        .catch((err) => console.log("err: ", err));
+        .catch(() => {
+          toast.error("Something went wrong, please try again");
+        });
     }
   };
+
   return (
-    <InputGroup size="md">
-      <Input
-        w={{ xl: "14rem", lg: "13rem", sm: "10rem", md: "11rem" }}
-        bg="offWhite"
-        placeholder="search collection..."
-        onChange={(event) => setValue(event.target.value.toLowerCase())}
-        value={value}
-      />
-      <InputRightAddon
-        bg="black"
-        transition="all 0.2s"
-        _active={{ transform: value.trim() !== "" && "scale(0.96)" }}
-        children={<GoSearch color="red" />}
-        onClick={searchCollection}
-      />
-    </InputGroup>
+    <>
+      <ToastContainer  style={{ width: "fit-content" }} position="top-center" />
+      <InputGroup size="md">
+        <Input
+          w={{ xl: "14rem", lg: "13rem", sm: "10rem", md: "11rem" }}
+          bg="offWhite"
+          placeholder="search collection..."
+          onChange={(event) => setValue(event.target.value.toLowerCase())}
+          value={value}
+        />
+        <InputRightAddon
+          bg="black"
+          transition="all 0.2s"
+          _active={{ transform: value.trim() !== "" && "scale(0.96)" }}
+          children={<GoSearch color="red" />}
+          onClick={searchCollection}
+        />
+      </InputGroup>
+    </>
   );
 }
